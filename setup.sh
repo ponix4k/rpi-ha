9c178e7fcc95702bb766eb4816aa0c3f7028e22b
John <2320804+ponix4k@users.noreply.github.com>
2024-09-18 18:17:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt auto-remove -y && \
 sudo apt install -y docker docker-compose && sudo usermod -aG docker $USER
 
 ## Create directories 
-mkdir ~/.HA && mkdir ~/.pihoile
+mkdir ~/.HA && mkdir ~/.pihole
 
 ## Copy yml files 
 cp ./ymls/DC-HA.yml ~/.HA/docker-compose.yml && \
